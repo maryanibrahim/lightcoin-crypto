14dbdab8556983690d597cd893b5e5ed9310bc57
Maryan ibrahim <53503272+maryanibrahim@users.noreply.github.com>
2023-06-06 03:31:47 -0500

    index.js

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+// Account class - This represents a bank account
+class Account {
+  
+  constructor() {
+    this.transactions = []; // This will hold all transactions for this account
   }
 
-  commit() {
-    balance -= this.amount;
+  // getter method to calculate the balance
+  get balance() {
+    let balance = 0; 
+    for (let transaction of this.transactions) {
+      balance += transaction.value; // For each transaction, we adjust the balance
+    }
+    return balance; // We return the calculated balance
   }
-
+  
+  addTransaction(transaction) {
+    this.transactions.push(transaction); // We add new transaction to this account's transactions array
+  }
 }
 
+// Transaction class - This is a base class for specific transaction types
+class Transaction {
+  
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
 
+  // When we commit a transaction, we add it to the account
+  commit() {
+    if (!this.isAllowed()) return false; // Check if the transaction is allowed
+    this.time = new Date(); // Record the time of the transaction
+    this.account.addTransaction(this); // Add the transaction to the account
+    return true; // Indicate the transaction was successful
+  }
+}
 
+// Withdrawal class - This class represents a withdrawal transaction
+class Withdrawal extends Transaction {
+  
+  get value() {
+    return -this.amount; // Withdrawals decrease the account balance
+  }
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+  // Check if the withdrawal is allowed
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0); // Make sure the account has enough balance to cover the withdrawal
+  }
+}
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+// Deposit class - This class represents a deposit transaction
+class Deposit extends Transaction {
+  
+  get value() {
+    return this.amount; // Deposits increase the account balance
+  }
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+  // Deposits are always allowed
+  isAllowed() {
+    return true;
+  }
+}
 
-console.log('Balance:', balance);
+// Now, we can test our code
+
+const myAccount = new Account(); // Create a new account
+
+// Try to withdraw money
+const t1 = new Withdrawal(100.00, myAccount);
+console.log(t1.commit()); // This should fail because the account has no money yet
+
+// Now, deposit some money and try again
+const t2 = new Deposit(200.00, myAccount);
+console.log(t2.commit()); // This should succeed
+
+// Now, try the withdrawal again
+const t3 = new Withdrawal(100.00, myAccount);
+console.log(t3.commit()); // This should now succeed
+
+// Print the final account balance
+console.log('Final account balance is: ', myAccount.balance);
